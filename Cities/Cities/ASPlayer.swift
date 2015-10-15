dcfa5c22c3d607c2eca19fbdb2574e4b05c156cf
Артем Соловьенко <a.solovenko@gmail.com>
2015-10-15 17:29:05 +0600

    first commit

---
+++ Cities/Cities/ASPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  ASPlayer.swift
+//  Cities
+//
+//  Created by Артем Соловьенко on 15.10.15.
+//  Copyright © 2015 Артем Соловьенко. All rights reserved.
+//
+
+import UIKit
+
+class ASPlayer: NSObject {
+
+}
